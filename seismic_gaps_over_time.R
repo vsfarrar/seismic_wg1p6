cb2648a1760ac0243be086a65dcfa495d377b4da
vsfarrar <vsfarrar@ucdavis.edu>
2021-09-01 13:37:05 -0700

    add demographic gaps by year, with or without summer terms
    ISSUE: the way year and summer is denoted in the crs_term is UC Davis specific. This code may not work for other institutions unless it is made clear that crs_term needs to be YYYYTT, where TT is a term corresponding 01 = winter, 03 =spring, 05/06/07 = summer, and 10 = fall.

--- seismic_gaps_over_time.R
+++ seismic_gaps_over_time.R
@@ -14,8 +14,43 @@ dat_new %>%
             mean_prior_gpa = mean(cum_prior_gpa),
             se_prior_gpa = std.error(cum_prior_gpa))
 
+
+demog_gaps_by_year <- 
+  dat_new %>%
+  pivot_longer(cols = c(female,ethnicode_cat,firstgen, lowincomeflag, transfer),
+               names_to = "demographic_var",
+               values_to = "value") %>%
+  mutate(crs_year = str_sub(crs_term, 1,4)) %>%
+  group_by(crs_name, crs_year, demographic_var, value) %>%
+  summarise(n = n(),
+            mean_grade = mean(numgrade),
+            sem_grade = std.error(numgrade), 
+            lower_ci = lower_ci(mean_grade,sem_grade,n, conf_level = 0.95), 
+            upper_ci = upper_ci(mean_grade,sem_grade,n, conf_level = 0.95),
+            mean_prior_gpa = mean(cum_prior_gpa),
+            se_prior_gpa = std.error(cum_prior_gpa))
+
+
+demog_gaps_by_year_no_summers <- 
+  dat_new %>%
+  pivot_longer(cols = c(female,ethnicode_cat,firstgen, lowincomeflag, transfer),
+               names_to = "demographic_var",
+               values_to = "value") %>%
+  mutate(crs_year = str_sub(crs_term, 1,4)) %>%
+  mutate(summer_term = ifelse(str_sub(crs_term,5,6) %in% c("05", "06", "07"), 1,0)) %>% 
+  filter(summer_term == 0) %>%
+  group_by(crs_name, crs_year, demographic_var, value) %>%
+  summarise(n = n(),
+            mean_grade = mean(numgrade),
+            sem_grade = std.error(numgrade), 
+            lower_ci = lower_ci(mean_grade,sem_grade,n, conf_level = 0.95), 
+            upper_ci = upper_ci(mean_grade,sem_grade,n, conf_level = 0.95),
+            mean_prior_gpa = mean(cum_prior_gpa),
+            se_prior_gpa = std.error(cum_prior_gpa))
+
 #export to a table 
 write.csv(demog_gaps_by_term, paste0("demographic_gaps_by_term_",current_date,".csv"))
+write.csv(demog_gaps_by_year, paste0("demographic_gaps_by_year_",current_date,".csv"))
 
 #### MEAN DIFFERENCES BY TERM ####
 
@@ -34,4 +69,5 @@ dat_new %>%
 #export to a table 
 write.csv(grade_gpa_diff, paste0("mean_grade_gpa_differences_by_term",current_date,".csv"))
 
-#ISSUE: This code does not work for the ethnicity group, as Asian is currently separated out 
+#ISSUE: This code does not work for the ethnicity group, as Asian is currently separated out
+#ISSUE: calculation of crs_year and summer terms are UC Davis entry specific. 
