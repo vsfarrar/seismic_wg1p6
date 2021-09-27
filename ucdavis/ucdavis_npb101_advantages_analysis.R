6048446eb05bf02b5f8fe79a834f19d03ddf3868
vsfarrar <vsfarrar@ucdavis.edu>
2021-09-27 13:00:23 -0700

    add covid quarter analyses to advantage plots
    per Natalia's email request, 9/27/21

--- ucdavis/ucdavis_npb101_advantages_analysis.R
+++ ucdavis/ucdavis_npb101_advantages_analysis.R
@@ -5,11 +5,20 @@ library(plotrix)
 #import SEISMIC formatted data
 dat <- read.csv("~/Documents/projects/dber_seismic/UCD_CBS_upper_division_SEISMIC-format_NPB_full_2021-09-13.csv", 
                   na.strings=c("","NA"))
-#Filter data
+#Filter data ####
 dat19 <- 
   dat %>%
   filter(summer_crs == 0) %>% #exclude summers
   filter(crs_term <= 201910) %>% #exclude COVID19 terms
+  #convert missing demographics to 0 (conservative)
+  tidyr::replace_na(list(ethnicode_cat = 0, firstgen = 0, international = 0,
+                         transfer = 0, lowincomeflag = 0))
+
+# Filter data to COVID only
+dat_covid <-
+  dat %>%
+  filter(summer_crs == 0) %>% #exclude summers
+  filter(crs_term > 201910) %>% #SELECT COVID terms
   #convert missing demographics to 0 (conservative)
   tidyr::replace_na(list(ethnicode_cat = 0, firstgen = 0, international = 0,
                          transfer = 0, lowincomeflag = 0))
@@ -21,16 +30,23 @@ npb101d <- dat19 %>%
          passed_npb101d = ifelse(lettergrade == "P", 1,0)) %>% #students that passed; n = 2470
   select(st_id, crs_term, enrolled_npb101d, passed_npb101d)
 
+npb101d_covid <- dat_covid %>%
+  filter(crs_name == "NPB101D" & !is.na(crs_term)) %>% #should be 3435 students
+  mutate(enrolled_npb101d = 1, #all students were enrolled
+         passed_npb101d = ifelse(lettergrade == "P", 1,0)) %>% #students that passed; n = 2470
+  select(st_id, crs_term, enrolled_npb101d, passed_npb101d)
+
 # How many students passed discussion? 
 npb101d %>% count(is_dfw, lettergrade)
   #general issue: lettergrade "NP" should count as dfw.
 
 # Join NPB101D data with original dataframe (dat19) ####
 dat19 <- left_join(dat19, npb101d) #default joins by st_id AND crs_term
+dat_covid <-left_join(dat_covid, npb101d_covid)
 
 # Calculate "Advantages" ####
-advantages_npb101d <- 
-  dat19 %>%
+advantages_npb101d_covid <- 
+  dat_covid %>%
   filter(crs_name == "NPB101" & crs_term >= 201610) %>%
   mutate(advantage = case_when(
     female == "0" & urm == "0" & firstgen == "0" & lowincomeflag == "0"  ~ "0",
@@ -81,11 +97,29 @@ advantages_npb101d <-
 
 # PLOT ####
 
-#averages, normalized to the "all advantages" group
+#raw grades in NPB 101
 advantages_npb101d %>%
+  ungroup() %>%
+  filter(!is.na(disadv_no)) %>%
+  mutate(group_label = fct_reorder(as.factor(group_label), advantage, min)) %>% 
+  #Plot
+  ggplot(aes(x = group_label, y = mean_grade, color = as.factor(enrolled_npb101d))) + 
+  geom_point(position = position_dodge(0.2)) + 
+  geom_errorbar(aes(ymin = mean_grade - se_grade, ymax = mean_grade + se_grade), width = 0.7,
+                position = position_dodge(0.2)) + 
+  geom_vline(xintercept = c(2.5, 5.5,11.5,15.5)) + 
+  labs(x = "No. of 'Advantages'", y = "Average Course Grade (NPB 101)", 
+       color = "Enrolled in NPB101D?", subtitle = "Fall 2016 - Fall 2019") + 
+  theme_classic(base_size = 14) + 
+  theme(legend.position = "bottom") + 
+  coord_flip()
+
+#averages, normalized to the "all advantages" group
+advantages_npb101d%>%
 ungroup() %>%
 filter(!is.na(disadv_no)) %>%
-  mutate(mean_grade_norm = ifelse(enrolled_npb101d == 0, mean_grade-2.849176,mean_grade-3.051852),
+  mutate(mean_grade_norm = ifelse(enrolled_npb101d == 0, mean_grade-2.849176,mean_grade-3.051852), 
+          #issue: need to code this dynamically, rather than hard coding
          group_label = fct_reorder(as.factor(group_label), advantage, min)) %>% 
   #Plot
   ggplot(aes(x = group_label, y = mean_grade_norm, color = as.factor(enrolled_npb101d))) + 
@@ -93,8 +127,10 @@ filter(!is.na(disadv_no)) %>%
   geom_errorbar(aes(ymin = mean_grade_norm - se_grade, ymax = mean_grade_norm + se_grade), width = 0.7,
                 position = position_dodge(0.2)) + 
   geom_vline(xintercept = c(2.5, 5.5,11.5,15.5)) + 
-  labs(x = "No. of 'Advantages'", y = "Average Course Grade \n(relative to most advantaged group)", color = "Enrolled in NPB101D?") + 
-  theme_classic(base_size = 16) + 
+  labs(x = "No. of 'Advantages'", y = "Average Course Grade \n(relative to most advantaged group)", 
+       color = "Enrolled in NPB101D?") + 
+  theme_classic(base_size = 14) + 
+  theme(legend.position = "bottom") + 
   coord_flip()
 
 # grade-gpa difference
@@ -111,4 +147,22 @@ advantages_npb101d %>%
   labs(x =  NULL, y = "Course Grade - Cum.Prior GPA \n(mean +/- SEM)", 
        color = "Enrolled in NPB101D?", subtitle = "Fall 2016 - Fall 2019") + 
   theme_classic(base_size = 16) + 
+  coord_flip()
+
+#PLOT COVID QUARTERS ####
+advantages_npb101d_covid %>%
+  ungroup() %>%
+  filter(!is.na(disadv_no)) %>%
+  mutate(#mean_grade_norm = ifelse(enrolled_npb101d == 0, mean_grade-2.849176,mean_grade-3.051852),
+    group_label = fct_reorder(as.factor(group_label), advantage, min)) %>% 
+  #Plot
+  ggplot(aes(x = group_label, y = mean_grade, color = as.factor(enrolled_npb101d))) + 
+  geom_point(position = position_dodge(0.2)) + 
+  geom_errorbar(aes(ymin = mean_grade - se_grade, ymax = mean_grade + se_grade), width = 0.7,
+                position = position_dodge(0.2)) + 
+  geom_vline(xintercept = c(2.5, 5.5,11.5,15.5)) + 
+  labs(x = "No. of 'Advantages'", y = "Average Course Grade", 
+       color = "Enrolled in NPB101D?", subtitle = "COVID19 quarters\n(Winter 2020 -Spring 2021)") + 
+  theme_classic(base_size = 14) + 
+  theme(legend.position = "bottom") + 
   coord_flip()
