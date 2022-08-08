e0272e2ad46af5b369a460ef42cefe137cd70b99
vsfarrar <vsfarrar@ucdavis.edu>
2022-08-08 14:11:41 -0700

    use gmodels for confidence intervals
    load gmodels package instead of hand-written confidence interval functions

--- seismic_setup.R
+++ seismic_setup.R
@@ -18,18 +18,10 @@ institution <- "UCD"
 
 #use pacman to install and load all packages required
 if (!require("pacman")) install.packages("pacman") #install pacman if not already installed
-pacman::p_load(tidyverse, dplyr,broom, plotrix, broom.mixed, lme4, lmerTest, robustlmm)
+pacman::p_load(tidyverse, dplyr,broom, plotrix, broom.mixed, lme4, lmerTest, robustlmm, gmodels)
 
 #grab current date using Sys.Date()
 current_date <- as.Date(Sys.Date(), format = "%m/%d/%Y")
-
-#confidence interval functions
-lower_ci <- function(mean, se, n, conf_level = 0.95){
-  lower_ci <- mean - qt(1 - ((1 - conf_level) / 2), n - 1) * se
-}
-upper_ci <- function(mean, se, n, conf_level = 0.95){
-  upper_ci <- mean + qt(1 - ((1 - conf_level) / 2), n - 1) * se
-}
 
 # make sure demographic variables are entered as character class
 dat <- dat %>% 
