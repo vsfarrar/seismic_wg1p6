13862dfba8a7ccc8a0bd2feacfbfc7218999c022
vsfarrar <vsfarrar@ucdavis.edu>
2022-10-08 15:55:21 -0700

    use rlmerRcpp function instead of rlmer
    rlmerRcpp function, along with the method = "DASvar", is more memory efficient

--- seismic_model_outputs_robust.R
+++ seismic_model_outputs_robust.R
@@ -8,8 +8,8 @@
 main_fx_all_rob <-
   dat_new %>%
   group_by(crs_name) %>% nest() %>%
-  mutate(fit = map(data, ~rlmer(numgrade ~ gpao + female + as.factor(ethniccode_cat) + firstgen + transfer + lowincomeflag +
-                               + (1|crs_offering), data = ., method = "DAStau")), 
+  mutate(fit = map(data, ~rlmerRcpp(numgrade ~ gpao + female + as.factor(ethniccode_cat) + firstgen + transfer + lowincomeflag +
+                               + (1|crs_offering), data = ., method = "DASvar")), 
          results = map(fit, tidy)) %>%
   unnest(results) %>%
   select(-data, -fit, - group) %>%
@@ -22,36 +22,18 @@ main_fx_all_rob <-
 main_fx_no_gpao_rob <-
   dat_new %>%
   group_by(university, crs_name) %>% nest() %>%
-  mutate(fit = map(data, ~rlmer(numgrade ~ female + as.factor(ethniccode_cat) + firstgen + transfer + lowincomeflag +
-                                + (1|crs_offering), data = ., method = "DAStau")), 
+  mutate(fit = map(data, ~rlmerRcpp(numgrade ~ female + as.factor(ethniccode_cat) + firstgen + transfer + lowincomeflag +
+                                + (1|crs_offering), data = ., method = "DASvar")), 
          results = map(fit, tidy)) %>%
   unnest(results) %>%
   select(-data, -fit, - group) %>%
   mutate(s.sig = case_when(statistic > 1.96 | statistic < -1.96 ~ "***",
                            statistic <= 1.96 & statistic >= -1.96 ~ "",)) 
 
-#produce ICC 
 
 #export models ####
 #export model outputs from each course
 write.csv(main_fx_all_rob, paste0(institution, "_mixed_model_outputs_main_effects_robust_",current_date,".csv"))
 write.csv(main_fx_no_gpao_rob, paste0(institution, "_mixed_model_outputs_noGPAO_robust_",current_date,".csv"))
 #write.csv(main_fx_urm_rob, paste0(institution, "_mixed_model_outputs_main_effects_urm_robust_",current_date,".csv"))
-
-
-##################
-#PREVIOUS CODE
-#mixed model with main effects, controlling for GPAO
-#race/ethnicity defined as urm
-
-#main_fx_urm_rob <-
-#  dat_new %>%
-#  group_by(university,crs_name) %>% nest() %>%
-#  mutate(fit = map(data, ~rlmer(numgrade ~ gpao + female + urm + firstgen + transfer + lowincomeflag +
-#                                + (1|crs_offering), data = ., method = "DAStau")), 
-#         results = map(fit, tidy)) %>%
-#  unnest(results) %>%
-#  select(-data, -fit, - group) %>%
-#  mutate(s.sig = case_when(statistic > 1.96 | statistic < -1.96 ~ "***",
-#                          statistic <= 1.96 & statistic >= -1.96 ~ "",)) 
 
