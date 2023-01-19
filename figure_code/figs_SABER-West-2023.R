cc6782e5f42e834ec571a0ffb9257e2075f956e3
vsfarrar <vsfarrar@ucdavis.edu>
2023-01-19 12:38:50 -0700

    SABER West figure code

--- figure_code/figs_SABER-West-2023.R
+++ figure_code/figs_SABER-West-2023.R
@@ -88,18 +88,18 @@ estims <-
   mutate(num_uni_topic = as.integer(as.factor(uni_topic)))  #convert to numeric for x breks
 
 #__plot####
-ggplot(estims, aes(x = num_uni_topic, y = estimate, 
-             color = as.factor(GPAO_included), shape = sig_topic)) + 
-  geom_point(size = 3) + 
+estims %>%
+ggplot(aes(x = num_uni_topic, y = estimate, 
+             color = as.factor(GPAO_included), shape = as.factor(significant))) + 
+  geom_point(size = 5) + 
   geom_errorbar(aes(ymin = estimate - std.error, 
                     ymax = estimate + std.error), 
                 width = 0.1) + 
   geom_hline(yintercept = 0) +
   geom_vline(xintercept = 5.5, color = "black", alpha = 0.9) + 
-  scale_shape_manual(values = c(1,16,2,17),
-                     labels = c("CellBio: n.s.", "CellBio: p < 0.05",
-                                "Genetics: n.s.", "Genetics: p < 0.05")) + 
-  scale_color_discrete(labels = c("no", "yes")) + 
+  scale_shape_manual(values = c(1,16),
+                     labels = c("n.s.", "p < 0.05")) + 
+  scale_color_manual(values = c("black", "red"), labels = c("no", "yes")) + 
   scale_x_continuous(minor_breaks = seq(1,10,by =1),
                      breaks = seq(1,10, by = 1),
                      labels = rep(deid_labels,2)) +
@@ -147,7 +147,7 @@ levels(sai_grades$uni_label) <- c("A", "B", "C","D","E")
   facet_wrap(~crs_topic) +
   theme_bw(base_size = 14) + 
   scale_size_continuous(trans = "log10", breaks = c(10,100,1000))+ #point size for readability
-  scale_fill_manual(values = alpha_colors) + 
+  scale_fill_manual(values = c("#66c2a5", "white", "white","white","white")) + 
   scale_y_continuous(breaks = seq(0,4,by = 1), limits = c(1,3.5)) + 
   labs(x = "No. of Systemic Advantages", y = "Course Grade", color = "Institution", fill = "Institution", size = "N") +
   theme_minimal(base_size = 20) + 
@@ -160,30 +160,40 @@ levels(sai_grades$uni_label) <- c("A", "B", "C","D","E")
   guides(fill = guide_legend(override.aes = list(size=4)))
 
 #__plot (grade anomaly) ####
+
+#reorder course topics
+sai$crs_topic <- factor(sai$crs_topic, 
+                               levels = c("Genetics", "CellBio"))
+#relabel order of institutions
+sai$uni_label <- recode_factor(sai$university,
+                                      "ASU" = "A", "UCD" = "B",
+                                      "IU" = "C", "Purdue" = "D", 
+                                      "UM" = "E")
+levels(sai$uni_label) <- c("A", "B", "C","D","E")
+
 sai %>%
   filter(category == "all") %>%
-  ggplot(aes(x = SAI, y = mean_grade_anomaly, color = university)) + 
+  ggplot(aes(x = SAI, y = mean_grade_anomaly, fill = uni_label)) + 
   geom_point(aes(size = n),
-             position = position_dodge(0.2)) + 
-  # geom_errorbar(aes(ymin = mean_grade_anomaly-se_grade_anomaly,
-  #                   ymax = mean_grade_anomaly+se_grade_anomaly),
-  #               width = 0,
-  #               position = position_dodge(0.2)) +
-  #geom_smooth(aes(fill= university, group = university), method = "lm", se = FALSE) + #correlation lines
+             position = position_dodge(0.2), 
+             alpha = 0.8, color = "black", pch = 21) +
   geom_hline(yintercept = 0) + 
   facet_wrap(~crs_topic) +
   theme_bw(base_size = 14) + 
   scale_size_continuous(trans = "log10", breaks = c(10,100,1000))+ #point size for readability
-  scale_color_manual(values = deid_colors, labels = deid_labels) + 
-  scale_fill_manual(values = deid_colors, labels = deid_labels) + 
-  labs(y = "Mean Grade Anomaly", color = "Institution", fill = "Institution", size = "N") +
-  theme_minimal(base_size = 14) + 
-  theme(axis.text = element_text(color = "black"), 
-        strip.text = element_text(color = "black"), 
+  scale_fill_manual(values = alpha_colors) + 
+  labs(y = "Mean Grade Anomaly", color = "Institution", fill = "Institution",
+       x = "No. of Systemic Advantages", size = "N") +
+  ylim(-1.10, 0) + 
+  theme_minimal(base_size = 20) + 
+  theme(axis.text = element_text(color = "black", size = 20), 
+        strip.text = element_text(color = "black", size = 24), 
         panel.border = element_rect(color = "black", fill = NA, size = 1), 
         strip.background = element_rect(color = "black", size = 1), 
         panel.grid.major.x = element_blank(),  #mask horizontal gridlines
-        panel.grid.minor.x = element_blank())
+        panel.grid.minor.x = element_blank()) + 
+  guides(fill = guide_legend(override.aes = list(size=4)))
+
 
 #__average numbers
 sai_grades %>%
