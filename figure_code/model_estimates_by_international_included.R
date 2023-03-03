5ff12c9203ece9e9579cbab4ab45712c300ae704
vsfarrar <vsfarrar@ucdavis.edu>
2023-03-02 21:55:46 -0700

    supplemental figure: model estimates by international inluded

--- figure_code/model_estimates_by_international_included.R
+++ figure_code/model_estimates_by_international_included.R
@@ -8,11 +8,14 @@ betas <- read.csv("~/Google Drive/My Drive/WG1P6/Processed Data/all_model_estima
   #points are pretty aligned
 betas %>%
   select(-X, -t.value) %>%
-  filter(variable != "GPAO") %>%
+  filter(variable %in% c("EthnicityBIPOC","Female","FirstGen","LowIncome", "Transfer")) %>%
+  mutate(variable = recode_factor(variable, EthnicityBIPOC = "PEER", Female = "Gender")) %>%
   pivot_wider(names_from = international_included, values_from = c(estimate,std.error,s.sig)) %>% 
   mutate(both_sig = ifelse(s.sig_0 == "***", 1, 0)) %>%
-  ggplot(aes(x = estimate_0, y = estimate_1, color = variable, shape = university)) +
+  ggplot(aes(x = estimate_0, y = estimate_1, color = university, shape = variable)) +
   geom_point(size = 3) + 
   geom_abline(intercept = 0, slope = 1, linetype = "dashed") + 
-  labs(x = "Model estimate: NO International students", y = "Model Estimate: International Students") + 
+  scale_color_manual(values = deid_colors, labels = deid_labels) +
+  labs(x = "Model estimate: NO International students", y = "Model Estimate: International Students",
+       color = "Institution", shape = "Variable") + 
   theme_bw()
