418271cc999105063fa1053910e57c0f996d93ea
vsfarrar <vsfarrar@ucdavis.edu>
2023-03-16 17:43:17 -0700

    reorder course subjects, renumber plot

--- figure_code/figVII_transfer-students.R
+++ figure_code/figVII_transfer-students.R
@@ -40,17 +40,21 @@ all_diff %>%
   ) 
 
 #__split facets####
-f6 <- splitFacet(transfdiff)
+fvii <- splitFacet(transfdiff)
 
 #C.model estimates ####
-fig6c <- 
+figViiC <- 
 betas %>% 
   filter(international_included == 1 & variable == "Transfer") %>%
+  mutate(crs_topic = factor(crs_topic, levels = c("Genetics", "CellBio"))) %>%
   mutate(significant = ifelse(is.na(s.sig), 0,1)) %>%
   mutate(uni_topic = paste(crs_topic, university, sep = "_"), #crs_topic 1st allows  x axis to be split by course
          sig_topic = paste(crs_topic, significant, sep = "_")) %>%
-  mutate(num_uni_topic = as.integer(as.factor(uni_topic))) %>% #convert to numeric for x breks
-  ggplot(aes(x = num_uni_topic, y = estimate, 
+  #create an x-axis placeholder 1-10 , by course within each variable and model
+  group_by(variable, GPAO_included) %>%
+  arrange(crs_topic) %>%
+  mutate(x_placeholder = row_number()) %>%
+  ggplot(aes(x = x_placeholder, y = estimate, 
              color = as.factor(GPAO_included), shape = sig_topic)) + 
   geom_point(size = 3) + 
   geom_errorbar(aes(ymin = estimate - std.error, 
@@ -58,16 +62,19 @@ betas %>%
                 width = 0.1) + 
   geom_hline(yintercept = 0) +
   geom_vline(xintercept = 5.5, color = "black", alpha = 0.9) + 
-  scale_shape_manual(values = c(1,16,2,17),
-                     labels = c("CellBio: n.s.", "CellBio: p < 0.05",
-                                "Genetics: n.s.", "Genetics: p < 0.05")) + 
+  scale_shape_manual(values = c("CellBio_0" = 1,"CellBio_1" = 16,
+                                "Genetics_0" = 2,"Genetics_1" = 17),
+                     #proper order in legend
+                     limits = c("Genetics_0", "Genetics_1", "CellBio_0", "CellBio_1"),
+                     labels = c("CellBio_0" = "CellBio: n.s.","CellBio_1" = "CellBio: p<0.05",
+                                "Genetics_0" = "Genetics: n.s.","Genetics_1" = "Genetics: p<0.05")) + 
   scale_color_discrete(labels = c("no", "yes")) + 
   scale_x_continuous(minor_breaks = seq(1,10,by =1),
                      breaks = seq(1,10, by = 1),
                      labels = rep(deid_labels,2)) +
   #annotate courses
-  annotate(geom = "text", x = 3, y = -0.7, label = "CellBio") + 
-  annotate(geom = "text", x = 8, y = -0.7, label = "Genetics") + 
+  annotate(geom = "text", x = 3, y = -0.7, label = "Genetics") + 
+  annotate(geom = "text", x = 8, y = -0.7, label = "CellBio") + 
   labs(x = NULL, y = NULL, shape = NULL, color = "GPAO in model?") + 
   theme_minimal(base_size = 14) + 
   theme(axis.text = element_text(color = "black"), 
@@ -79,20 +86,20 @@ betas %>%
         legend.position =  "right")
 
 #cowplot ####
-fig6 <- cowplot::plot_grid(f6[[1]] + labs(subtitle = "CellBio", y = "Course Grade Difference",
+figVII <- cowplot::plot_grid(fvii[[2]] + labs(subtitle = "Genetics", y = "Course Grade Difference",
                                             x = "GPAO Difference") + sharedx + sharedy + 
-                             annotate(geom = "text", x=Inf, y = -Inf, label = "2.30%",
-                                      vjust = -2, hjust = 1.5),
-                           f6[[2]] + labs(subtitle = "Genetics", y = NULL, x = "GPAO Difference") + sharedx + sharedy + 
                              annotate(geom = "text", x=Inf, y = -Inf, label = "7.30%",
                                       vjust = -2, hjust = 1.5),
-                           fig6c + labs(y = "Estimate ± SE", x = "Institution"),
+                           fvii[[1]] + labs(subtitle = "CellBio", y = NULL, x = "GPAO Difference") + sharedx + sharedy + 
+                             annotate(geom = "text", x=Inf, y = -Inf, label = "2.30%",
+                                      vjust = -2, hjust = 1.5),
+                           figViiC + labs(y = "Estimate ± SE", x = "Institution"),
                            labels = "AUTO", nrow = 1, ncol = 3, align = "h", axis = "bt", 
                            rel_widths = c(1.2,1,2))
 #get legend for bottom 
-fig6legend <- cowplot::get_legend(f6[[2]] + labs(fill = "Institution") + theme(legend.position = "bottom"))
+figVIIlegend <- cowplot::get_legend(fvii[[2]] + labs(fill = "Institution") + theme(legend.position = "bottom"))
 
 #export plot ####
-ggsave(filename = paste0("fig6_transfer_students_", current_date, ".png"), path = "figures/",
-       fig6, height = 490/96, width = 1110/96, units = "in", dpi = 300, bg= "white")
+ggsave(filename = paste0("figVII_transfer_students_", current_date, ".png"), path = "figures/",
+       figVII, height = 490/96, width = 1110/96, units = "in", dpi = 300, bg= "white")
 
