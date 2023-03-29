8b2a917d5757983eef0364d60e30966d3f60af22
vsfarrar <vsfarrar@ucdavis.edu>
2023-03-29 10:47:22 -0700

    change model call to use `PEER` instead of `ethniccode_cat`
    -use "PEER" in model call instead of "ethniccode_cat". Comparisons of PEER group will now be to both White (cat == 0) and Asian (cat == 2) instead of only white.

--- seismic_model_outputs_robust.R
+++ seismic_model_outputs_robust.R
@@ -3,12 +3,12 @@
 #rlmer(All main fx)####
 #run a mixed model wtih all main effects only, each offering as a random effect, for each course
   #controlling for GPAO 
-  #race/ethnicity defined as ethniccode_cat
+  #race/ethnicity defined via the PEER variable (PEER: ethniccode_cat == 1)
 
 main_fx_all_rob <-
   dat_new %>%
   group_by(crs_name) %>% nest() %>%
-  mutate(fit = map(data, ~rlmerRcpp(numgrade ~ gpao + female + as.factor(ethniccode_cat) + firstgen + transfer + lowincomeflag +
+  mutate(fit = map(data, ~rlmerRcpp(numgrade ~ gpao + female + peer + firstgen + transfer + lowincomeflag +
                                + (1|crs_offering), data = ., method = "DASvar")), 
          results = map(fit, tidy)) %>%
   unnest(results) %>%
@@ -22,7 +22,7 @@ main_fx_all_rob <-
 main_fx_no_gpao_rob <-
   dat_new %>%
   group_by(university, crs_name) %>% nest() %>%
-  mutate(fit = map(data, ~rlmerRcpp(numgrade ~ female + as.factor(ethniccode_cat) + firstgen + transfer + lowincomeflag +
+  mutate(fit = map(data, ~rlmerRcpp(numgrade ~ female + peer + firstgen + transfer + lowincomeflag +
                                 + (1|crs_offering), data = ., method = "DASvar")), 
          results = map(fit, tidy)) %>%
   unnest(results) %>%
@@ -31,9 +31,9 @@ main_fx_no_gpao_rob <-
                            statistic <= 1.96 & statistic >= -1.96 ~ "",)) 
 
 
+
 #export models ####
 #export model outputs from each course
 write.csv(main_fx_all_rob, paste0(institution, "_mixed_model_outputs_main_effects_robust_",current_date,".csv"))
 write.csv(main_fx_no_gpao_rob, paste0(institution, "_mixed_model_outputs_noGPAO_robust_",current_date,".csv"))
-#write.csv(main_fx_urm_rob, paste0(institution, "_mixed_model_outputs_main_effects_urm_robust_",current_date,".csv"))
 
