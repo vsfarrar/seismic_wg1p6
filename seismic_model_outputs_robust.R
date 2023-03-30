afe80981afbafd62aafc0fe59f0467fa00d9e38b
vsfarrar <vsfarrar@ucdavis.edu>
2023-03-30 10:37:31 -0700

    add second-order interaction models
    adds models:
    - all vars*GPAO
    - female*GPAO only
    - no interactions, using prior GPA instead of GPAO (for supplemental)

--- seismic_model_outputs_robust.R
+++ seismic_model_outputs_robust.R
@@ -30,8 +30,8 @@ main_fx_no_gpao_rob <-
   mutate(s.sig = case_when(statistic > 1.96 | statistic < -1.96 ~ "***",
                            statistic <= 1.96 & statistic >= -1.96 ~ "",)) 
 
-#rlmer(no GPAO)####
-#robust mixed model with all main effects WITHOUT GPAO CONTROL
+#rlmer: grade_anomaly as dependent var####
+#robust mixed model with all main effects, but with grade anomaly as the dependent variable
 
 grade_anom_main_fx <-
   dat_new %>%
@@ -45,10 +45,57 @@ grade_anom_main_fx <-
   mutate(s.sig = case_when(statistic > 1.96 | statistic < -1.96 ~ "***",
                            statistic <= 1.96 & statistic >= -1.96 ~ "",)) 
 
+#rlmer: interactions model, all vars*gpao ####
+#robust mixed model with interactions between all variables and GPAO
+#this will allow us to see if any variables significantly interact with GPAO in the dataset
+
+interactions_gpao_rob <-
+  dat_new %>%
+  group_by(university, crs_name) %>% nest() %>%
+  mutate(fit = map(data, ~rlmerRcpp(numgrade ~ female*gpao + peer*gpao + firstgen*gpao + transfer*gpao + lowincomeflag*gpao + international*gpao +
+                                      + (1|crs_offering), data = ., method = "DASvar")), 
+         results = map(fit, tidy)) %>%
+  unnest(results) %>%
+  select(-data, -fit, - group) %>%
+  mutate(s.sig = case_when(statistic > 1.96 | statistic < -1.96 ~ "***",
+                           statistic <= 1.96 & statistic >= -1.96 ~ "",)) 
+
+#rlmer: interactions between only gender*GPAO ####
+#robust mixed model with interaction between gender and GPAO only
+#allows for comparison of information criteria between main fx only and gender*GPAO
+
+interactions_gpao_gender <-
+  dat_new %>%
+  group_by(university, crs_name) %>% nest() %>%
+  mutate(fit = map(data, ~rlmerRcpp(numgrade ~ female*gpao + peer + firstgen + transfer + lowincomeflag + international +
+                                      + (1|crs_offering), data = ., method = "DASvar")), 
+         results = map(fit, tidy)) %>%
+  unnest(results) %>%
+  select(-data, -fit, - group) %>%
+  mutate(s.sig = case_when(statistic > 1.96 | statistic < -1.96 ~ "***",
+                           statistic <= 1.96 & statistic >= -1.96 ~ "",)) 
+
+#rlmer: prior GPA instead of GPAO ####
+#robust mixed model with prior GPA instead of GPAO
+#for supplemental in the MS
+
+main_fx_prior_gpa_rob<-
+  dat_new %>%
+  group_by(university, crs_name) %>% nest() %>%
+  mutate(fit = map(data, ~rlmerRcpp(numgrade ~ cum_prior_gpa + female + peer + firstgen + transfer + lowincomeflag + international +
+                                      + (1|crs_offering), data = ., method = "DASvar")), 
+         results = map(fit, tidy)) %>%
+  unnest(results) %>%
+  select(-data, -fit, - group) %>%
+  mutate(s.sig = case_when(statistic > 1.96 | statistic < -1.96 ~ "***",
+                           statistic <= 1.96 & statistic >= -1.96 ~ "",)) 
 
 #export models ####
 #export model outputs from each course
 write.csv(main_fx_all_rob, paste0(institution, "_mixed_model_outputs_main_effects_robust_",current_date,".csv"))
 write.csv(main_fx_no_gpao_rob, paste0(institution, "_mixed_model_outputs_noGPAO_robust_",current_date,".csv"))
 write.csv(grade_anom_main_fx, paste0(institution, "_mixed_model_grade_anomaly_main_fx_robust_",current_date,".csv"))
+write.csv(interactions_gpao_rob, paste0(institution, "_mixed_model_interactions_all-vars-x-GPAO_",current_date,".csv"))
+write.csv(interactions_gpao_gender, paste0(institution, "_mixed_model_interactions_gender-x-GPAO_",current_date,".csv"))
+write.csv(main_fx_prior_gpa_rob, paste0(institution, "_mixed_model_outputs_main_fx_prior-gpa_",current_date,".csv"))
 
